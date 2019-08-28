9dc07a963f79672138b88ca6e6b2c3ae070b502f
randomslap <kentokazaki@gmail.com>
2019-08-28 12:55:22 -0700

    added react-bootstrap; added carousel to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { Carousel } from 'react-bootstrap';
 
 class App extends Component {
+  state = {
+    index: 0
+  }
   render() {
     return (
-      <div className="App">
-      </div>
+      <Carousel controls={false} touch={false} slide={false} fade={true}>
+        <Carousel.Item>
+          Hello
+        </Carousel.Item>
+        <Carousel.Item>
+          Hello
+        </Carousel.Item>
+      </Carousel>
     );
   }
 }
