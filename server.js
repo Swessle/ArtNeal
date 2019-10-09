7b153f6d033a9d29b7af7c0374325e4d05b555b1
randomslap <kentokazaki@gmail.com>
2019-10-08 17:04:02 -0700

    modified server for heroku

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
-}
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
 	app.get("/*", function(req, res) {
 		res.sendFile(path.join(__dirname, "./client/build/index.html"));
 	});
