bd1816f1eb8671f3753717e5fc4e11b71dced325
Logan Bertrand <loganwbertrand@gmail.com>
2019-11-26 15:05:58 -0800

    renamed photos, next is categorizing them to import into the gallery page

---
+++ client/src/components/Gallery/gallery.js
@@ -0,0 +1,28 @@
+const images = [
+    {
+        id: 1,
+        name: "SpongeBob",
+        image: '../../pages/assets/images/4Jazz.jpg',
+        category:"Jazz"
+      },
+      {
+        id: 2,
+        name: "Sponge",
+        image: '../../pages/assets/images/IMG_0035.jpg',
+        category:"Nude"
+      },
+      {
+        id: 3,
+        name: "Bob",
+        image: '../../pages/assets/images/IMG_0036.jpg',
+        category:"Nude"
+      },
+      {
+        id: 4,
+        name: "Spon",
+        image: '../../pages/assets/images/IMG_0037.jpg',
+        category:"Jazz"
+      },
+]
+
+export default images;
