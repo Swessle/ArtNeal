4396f71833e531f1b39c8c427fc9b2d3ecec7de4
Madrigalbit <madrigalbit@gmail.com>
2019-12-05 04:03:33 -0800

    added basic footer and folder set up, footer is being displayed just needs work

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../../pages/index.css";
+
+const Footer = () => {
+	return (
+		<Container id="footer">
+			<Row>
+				<Col>
+					<p>TEST//TEST</p>
+				</Col>
+			</Row>
+		</Container>
+	);
+};
+
+export default Footer;
