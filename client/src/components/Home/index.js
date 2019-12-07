4d16b2ae3ea43686749122513db2d401d8083b44
randomslap <kentokazaki@gmail.com>
2019-12-06 22:27:24 -0800

    navbar fix

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -6,7 +6,7 @@ import { withRouter } from "react-router-dom";
 import firstImage from "../../pages/assets/images/IMG_2624.jpg";
 import secondImage from "../../pages/assets/images/IMG_2634.jpg";
 import thirdImage from "../../pages/assets/images/IMG_2650.jpg";
-import "./index.css"
+import "./index.css";
 
 class Home extends Component {
 	constructor(props, context) {
@@ -20,6 +20,13 @@ class Home extends Component {
 		};
 	}
 
+	componentDidMount() {
+		if (this.props.location.pathname !== "/") {
+			document.body.classList.add("darkNav");
+		} else if (this.props.location.pathname === "/") {
+			document.body.classList.remove("darkNav");
+		}
+	}
 	handleSelect(selectedIndex, e) {
 		this.setState({
 			index: selectedIndex,
@@ -34,12 +41,16 @@ class Home extends Component {
 	render() {
 		const { index, direction } = this.state;
 		const LandingButton = () => {
-			return <button id="landingbtn" onClick={this.onChangeAbout}>View Gallery</button>;
+			return (
+				<button id="landingbtn" onClick={this.onChangeAbout}>
+					View Gallery
+				</button>
+			);
 		};
 		const LandingText = () => {
-			return <h1 id="landingtext">Neal Lange</h1>
-		}
-		
+			return <h1 id="landingtext">Neal Lange</h1>;
+		};
+
 		return (
 			<>
 				<Carousel
@@ -63,13 +74,12 @@ class Home extends Component {
 						<Carousel.Caption id="textcaption">
 							<LandingText />
 						</Carousel.Caption>
-						
+
 						<Carousel.Caption id="btncaption">
 							<LandingButton />
 						</Carousel.Caption>
-					
 					</Carousel.Item>
-					
+
 					<Carousel.Item>
 						<img
 							className="d-block w-100"
@@ -81,13 +91,12 @@ class Home extends Component {
 						<Carousel.Caption id="textcaption">
 							<LandingText />
 						</Carousel.Caption>
-						
+
 						<Carousel.Caption id="btncaption">
 							<LandingButton />
 						</Carousel.Caption>
-					
 					</Carousel.Item>
-					
+
 					<Carousel.Item>
 						<img
 							className="d-block w-100"
@@ -99,13 +108,11 @@ class Home extends Component {
 						<Carousel.Caption id="textcaption">
 							<LandingText />
 						</Carousel.Caption>
-						
+
 						<Carousel.Caption id="btncaption">
 							<LandingButton />
 						</Carousel.Caption>
-					
 					</Carousel.Item>
-				
 				</Carousel>
 			</>
 		);
@@ -116,9 +123,4 @@ const mapStateToProps = state => ({
 	slide: state.slide
 });
 
-export default withRouter(
-	connect(
-		mapStateToProps,
-		{ setAbout }
-	)(Home)
-);
+export default withRouter(connect(mapStateToProps, { setAbout })(Home));
